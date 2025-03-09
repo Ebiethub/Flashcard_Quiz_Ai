323fbd8245ab6c1217c0e3fc0fb1795e139ccb73
Ebiet Matthew Monday <151534400+Ebiethub@users.noreply.github.com>
2025-03-09 17:44:03 +0100

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from utils import extract_text, chunk_text
 load_dotenv()
 
 # Initialize Groq model
-GROQ_API_KEY = "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"
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 groq_chat = ChatGroq(
         groq_api_key=GROQ_API_KEY,
         model_name="llama-3.3-70b-specdec",
